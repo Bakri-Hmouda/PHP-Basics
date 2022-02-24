fe4a79a25d452eb4ded36bed88a50bd5cd5c98fc
Bakrifly <bakri.hmouda@gmail.com>
2022-02-24 12:26:31 +0200

    Step 3 : Variables lvl 1

--- step_1.php
+++ step_1.php
@@ -1,7 +1,19 @@
 "Hello world, Help me I am using PHP"
 <?php
-"I am getting errors but I can't see the errors"
-?>
-<?php
-echo("Search on how to enable PHP Errors")  ;
+"I am getting errors but I can't see the errors";
+
+echo "Search on how to enable PHP Errors\n";
+
+
+$name = 'Bakri';
+$email = 'bakri.hmouda@gmail.com';
+
+echo "My name is $name and my email is $email\n";
+
+$height = 10;
+$width = 10;
+$area = $height * $width;
+
+echo "area is: $area\n";
+
 ?>
