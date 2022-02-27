80121757883095a4ca9a2e105f17a22ab82eda83
Bakrifly <bakri.hmouda@gmail.com>
2022-02-27 18:15:17 +0200

    Step 4_2

---
+++ Basics-02/step_4.php
@@ -0,0 +1,18 @@
+<?php
+$num=1;  
+$total=0;  
+$x=$num;  
+while($x!=0)  
+{  
+$rem=$x%10;  
+$total=$total+$rem*$rem*$rem;  
+$x=$x/10;  
+}  
+if($num==$total)  
+{  
+echo "true";  
+}  
+else  
+{  
+echo "false";  
+}
